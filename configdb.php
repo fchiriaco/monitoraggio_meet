3ffd57566e72b5fcb20e15f5499fcbc70991152c
Francesco Chiriaco <posta@francescochiriaco.it>
2020-05-10 10:00:17 +0200

    Personalizzazione configdb.php

--- configdb.php
+++ configdb.php
@@ -1,20 +1,35 @@
 <?php
-
+/*
+$hostmysql = "62.149.150.65";
+$usermysql = "Sql120708";
+$pwdmysql = "290392f5";
+$dbmysql = "Sql120708_4";
+$tabella_codici = "codici_auth";
+$token = "fchiriaco_1965";
+*/
 
 define("HOST","localhost");
-define("USER","myuser");
-define("PWD","mypassword");
-define("DBNAME","mydb");
+define("USER","root");
+define("PWD","Fgm172826#");
+define("DBNAME","monmeet");
 
 
 
 $hostmysql = "localhost";
-$usermysql = "myuser";
-$pwdmysql = "mypassword";
-$dbmysql = "mydb";
+$usermysql = "root";
+$pwdmysql = "Fgm172826#";
+$dbmysql = "monmeet";
 $tabella_codici = "codici_auth";
-$token = "Aaxertdfg41987";
+$token = "fchiriaco_1965";
 
 
+/*
+$hostmysql = "localhost";
+$usermysql = "iisvicouser";
+$pwdmysql = "dddkxj3dfhccc";
+$dbmysql = "iisvicodb";
+$tabella_codici = "codici_auth";
+$token = "fchiriaco_1965";
+*/
 
 ?>
